39bd409161d4df4e08825195ebdb8f25fdf2d9e6
BarqDB <julfikaronthego@gmail.com>
2026-07-10 13:49:43 +0800

    docs: add Barq copyright to the two modified jni_utils files
    These Android JNI helpers were modified during the fork (+50 / +57 lines)
    but were the only tracked source files still missing the Barq copyright
    line that the other 200+ rebranded files carry. Add "Copyright (c) 2026
    the Barq authors" directly under the retained "Copyright 2018 Realm Inc."
    header, matching the repo convention. Realm's notice is unchanged.

--- packages/barq/binding/android/src/main/cpp/jni_utils.cpp
+++ packages/barq/binding/android/src/main/cpp/jni_utils.cpp
@@ -1,5 +1,6 @@
 /*
  * Copyright 2018 Realm Inc.
+ * Copyright (c) 2026 the Barq authors
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
